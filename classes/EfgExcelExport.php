6b77907969864ee059dca4b108aa98c94e16307c
hschottm <helmut.schottmueller@me.com>
2015-07-31 09:58:30 +0200

    Fixed issues with missing form variable

--- classes/EfgExcelExport.php
+++ classes/EfgExcelExport.php
@@ -1,29 +1,11 @@
 <?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
 
 /**
- * TYPOlight webCMS
- * Copyright (C) 2005 Leo Feyer
+ * Contao Open Source CMS
  *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at http://www.gnu.org/licenses/.
+ * Copyright (c) 2005-2015 Leo Feyer
  *
- * PHP version 5
- * @copyright  Leo Feyer 2005
- * @author     Leo Feyer <leo@typolight.org>
- * @package    News
- * @license    LGPL
- * @filesource
+ * @license LGPL-3.0+
  */
 
 
@@ -31,8 +13,8 @@
  * Class EfgExcelExport
  *
  * Excel 2003/2007 export for EFG using PHPExcel library
- * @copyright  Helmut Schottmüller 2008
- * @author     Helmut Schottmüller <helmut.schottmueller@mac.com>
+ * @copyright  Helmut Schottmüller 2008-2015
+ * @author     Helmut Schottmüller <hschottm@gmail.com>
  * @package    Controller
  */
 class EfgExcelExport extends Backend
@@ -54,16 +36,18 @@ class EfgExcelExport extends Backend
 	 */
 	public function export($headers, $data)
 	{
+		if (!is_array($data) || count($data) == 0) return;
+                $formtitle_natural = $data[1]["form"];
+
 		// Create new PHPExcel object
 		$objPHPExcel = new \PHPExcel();
-		
 		// Set properties
-		$objPHPExcel->getProperties()->setCreator("TYPOlight Web CMS");
-		$objPHPExcel->getProperties()->setLastModifiedBy("TYPOlight Web CMS");
-		$objPHPExcel->getProperties()->setTitle($this->strFormKey);
-		$objPHPExcel->getProperties()->setSubject($this->strFormKey);
-		$objPHPExcel->getProperties()->setDescription($this->strFormKey);
-		$objPHPExcel->getProperties()->setKeywords("office 2007 TYPOlight CMS");
+		$objPHPExcel->getProperties()->setCreator("Contao");
+		$objPHPExcel->getProperties()->setLastModifiedBy("Contao");
+		$objPHPExcel->getProperties()->setTitle($formtitle_natural);
+		$objPHPExcel->getProperties()->setSubject($formtitle_natural);
+		$objPHPExcel->getProperties()->setDescription($formtitle_natural);
+		$objPHPExcel->getProperties()->setKeywords("office 2007 Contao");
 		$objPHPExcel->getProperties()->setCategory("form input data");
 		
 		$objPHPExcel->setActiveSheetIndex(0);
@@ -117,13 +101,11 @@ class EfgExcelExport extends Backend
 		);
 		
 		// Rename sheet
-		$formtitle = $data[1]["form"];
-		$filename = escapeshellcmd(str_replace(" ", "_", $formtitle));
-		$formtitle = utf8_substr($formtitle, 31);
-		$objPHPExcel->getActiveSheet()->setTitle($formtitle);
+		$filename = escapeshellcmd(str_replace(" ", "_", $formtitle_natural));
+		$formtitle_natural== utf8_substr($formtitle_natural, 0, 31);
+		$objPHPExcel->getActiveSheet()->setTitle($formtitle_natural);
 		// Set active sheet index to the first sheet, so Excel opens this as the first sheet
 		$objPHPExcel->setActiveSheetIndex(0);
-		
 				
 		// Save Excel 2007 file
 		$objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
